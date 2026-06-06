72245b41b7caadb76a56f2a7fa38683c39befcc9
Maxforo <makssky125@gmail.com>
2026-06-06 23:11:32 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+import datetime, requests
+
+app = Flask(__name__)
+# Твоя рабочая ссылка CallMeBot
+URL = "https://api.callmebot.com/text.php?user=@maksforo&text=TEXT&apikey=8872428"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/book", methods=["POST"])
+def book():
+    name = request.form.get("username")
+    contact = request.form.get("contact")
+    bungalow = request.form.get("bungalow") # Принимаем выбранное бунгало
+    
+    # Сохраняем в bookings.txt
+    data = f"{datetime.datetime.now()} | {name} | {contact} | {bungalow}\n"
+    with open("bookings.txt", "a", encoding="utf-8") as f:
+        f.write(data)
+        
+    # Отправляем красивое уведомление в Телеграм
+    try:
+        msg = f"Новая бронь!\nИмя: {name}\nКонтакты: {contact}\nДомик: {bungalow}"
+        requests.get(URL.replace("TEXT", msg))
+    except:
+        pass
+        
+    return redirect(url_for('success'))
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
